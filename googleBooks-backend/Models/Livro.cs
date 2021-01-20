605a00549cbe651a743680ec10d584180b34e5c9
edwilson.cruz <k2eddwilson@gmail.com>
2021-01-20 16:47:05 -0300

    Inicio do projeto, e add package.

---
+++ googleBooks-backend/Models/Livro.cs
@@ -0,0 +1,11 @@
+namespace googleBooks_backend.Models
+{
+  public class Livro
+  {
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public string ImagemThumbnail { get; set; }
+    public DateTime DataEntrada { get; set; }
+  }
+}
